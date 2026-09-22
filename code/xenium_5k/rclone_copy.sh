149c6fabfa16dae8015bc031f6f3bcd92c5376ad
khuongduying <khuongduying@gmail.com>
2026-09-23 06:59:27 +0700

    update

---
+++ code/xenium_5k/rclone_copy.sh
@@ -0,0 +1,11 @@
+# From HPC
+rclone copy \
+    results/xenium_5k/visualization/ \
+    gdrive:xenium_project/xenium_lung/results/xenium_5k/visualization/ \
+    -P
+
+# From local
+rclone copy \
+    results/xenium_5k/visualization/ \
+    gdrive:xenium_project/xenium_lung/results/xenium_5k/visualization/ \
+    -P
